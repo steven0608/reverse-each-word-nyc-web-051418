07a30cc8d10132399a24cb780c4e2bd5c7ef4778
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 15:40:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,10 +4,10 @@ def reverse_each_word(sentence)
     reverse_word << x.reverse
   end
   reverse_word.join(" ")
-end
 
-def reverse_each_word(sentence)
   sentence.split().collect do |x|
   x.reverse
   end
 end
+
+
